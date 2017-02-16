ae6a1fa3139a4110cdeefc9555b4a64800f88101
Kerthil <kerthyl@gmail.com>
2017-02-15 23:36:32 -0500

    test

--- items.py
+++ items.py
@@ -1,11 +1,13 @@
 class Item(object):
-    def __init__(self, name, des):
-        self.name = name
-        self.des = description
+    def __init__(self, name, des, qoh=0):
+        self.name = name #Item name
+        self.des = description #Item Description
+        self.qoh = q #Quanity of item
 
 
-def gen_item(id):
+def add_item(id):
     if id == 1: #Potion
         I1 = Item("Potion", "A simple potion used to heal 5 hp.")
     if id == 2: #Super Potion
         I1 = Item("Super Potion", "An advanced potion used to heal 10 hp.")
+

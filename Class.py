34cf058483c86140ff0cca01cf7052edc764239f
Kerthil <kerthyl@gmail.com>
2017-02-17 22:52:01 -0500

    created enemy factory
    :D

--- Class.py
+++ Class.py
@@ -1,3 +1,4 @@
+import random
 class Character(object):
     def __init__(self, name, atk, arm, mhp):
         self.name = name
@@ -16,11 +17,74 @@ class Character(object):
 
 
 class Player(Character):
+    def __init__(self,name,atk,arm,mhp, xp=0):
+        super(Player, self).__init__(name,atk,arm,mhp)
+        self.xp = xp
 
-class Enemy(Character):
+    def set_xp(self, gainz):  #Sets new xp after battles.
+        xp += gainz
+        self.level_up()
+
+    def level_up(self): #Checks if level up, if so goto level up screen
+        pass
+
+
+class Enemy_factory(object): #Build enemies in a super efficient way
+
+    """
+    " EXAMPLES
+    " self.enemy_lookup_table['Trent']['zone'] = ['forest'] # this means the trent is found in the forest
+    " self.enemy_lookup_table['Spider']['zone'] = ['forest','cave'] # this means the spider is found in the cave and forest
+    " TO GENERATE AN ENEMY...
+    "   new_enemy = Character(self.enemy_lookup_table['Trent']['stats'][0],self.enemy_lookup_table['Trent']['stats'][1],
+    "   self.enemy_lookup_table['Trent']['stats'][2],self.enemy_lookup_table['Trent']['stats'][3])
+    """
+
+    def __init__(self):
+        self.enemy_lookup_table = {
+            'Trent': {
+                'zone': ['forest', ],
+                'stats': ['Trent', 1, 1, 1, 1]},
+            'Spider': {
+                'zone': ['forest', 'cave', ],
+                'stats': ['Spider', 1, 1, 1, 1]},
+            'Piranha Plant': {
+                'zone': ['forest', ],
+                'stats': ['Piranha Plant', 1, 1, 1, 1]},
+            'Bat': {
+                'zone': ['cave', ],
+                'stats': ['Bat', 1, 1, 1, 1]},
+            'Skeleton': {
+                'zone': ['cave', ],
+                'stats': ['Skeleton', 1, 1, 1, 1]},
+        }
+
+    def generate_enemy(self, enemy):
+        return Character(enemy[0], enemy[1], enemy[2], enemy[3])
+
+    def generate_wave(self, mission_enemy_table, floor):
+        enemy_wave = []
+        for i in range(3):
+            enemy_stat_mult = 1.5 * floor
+            for stat in mission_enemy_table[i][1:]:
+                stat *= enemy_stat_mult
+            enemy_wave.append(self.generate_enemy(random.choice(mission_enemy_table)))
+        return enemy_wave
+
+    def generate_mission(self, zone, floor):
+        mission_enemy_table = []
+        enemy_waves = []
+        for key in self.enemy_lookup_table:
+            if zone in self.enemy_lookup_table[key]['zone']:
+                mission_enemy_table.append(self.enemy_lookup_table[key]['stats'])
+        print mission_enemy_table
+        for wave in range(3):
+            enemy_waves.append(self.generate_wave(mission_enemy_table, floor))
+        return enemy_waves
+
 
 class Boss(Character):
-
+    pass
 
 
 def damage(atk,arm): #Calculates damage
@@ -29,18 +93,18 @@ def damage(atk,arm): #Calculates damage
 
 
 
+if __name__ == "__main__":
+    Kerthil = Player("Kerthil", 5, 2, 10)
 
-Kerthil = Player("Kerthil", 5, 2, 10)
+    print Kerthil.atk
+    print Kerthil.arm
+    print Kerthil.mhp
+    print Kerthil.hp
 
-print Kerthil.atk
-print Kerthil.arm
-print Kerthil.mhp
-print Kerthil.hp
+    Kerthil.take_damage(2)
 
-Kerthil.take_damage(2)
+    print Kerthil.hp
 
-print Kerthil.hp
-
-Zgrill = Player("Zgrill",2,3,12)
+    Zgrill = Player("Zgrill",2,3,12)
 
 
--- items.py
+++ items.py
@@ -4,6 +4,11 @@ class Item(object):
         self.des = description
 
 
+
+"""
+"To Do:
+"1. check if object generated already, add on hand.
+"""
 def gen_item(id):
     if id == 1: #Potion
         I1 = Item("Potion", "A simple potion used to heal 5 hp.")
